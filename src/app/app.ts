38e8eada398715832baadb03b12a90ae06d8e48a
ofmarin <ofelmaran@gmail.com>
2026-05-07 19:35:43 -0400

    misc changes

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,7 +4,6 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrls: ['./app.css']
 })
